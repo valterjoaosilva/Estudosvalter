3ddc3a0dffae131bd05197ab2c6a708189a5b154
V008 <V008@BNU-ESTAG008>
2018-07-12 16:19:47 -0300

    alteração multiplos de 3

---
+++ T1/multiplosde3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multiplosde3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    MessageBox.Show("O numero é:" + i + "é um multiplo de 3");
+                }
+            }
+
+        }
+    }
+}
