091bfa266bcd57e9b285470dd292a16cd3061cee
V008 <V008@BNU-ESTAG008>
2018-07-12 15:20:44 -0300

    valor da nota fiscal, novo exercicio

---
+++ T1/valordanotafiscal/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace valordanotafiscal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double valordanotafiscal = 3250;
+            if (valordanotafiscal <= 999)
+            {
+                MessageBox.Show("Imposto deve ser 2%");
+            }
+            else if (valordanotafiscal >= 1000 && valordanotafiscal <= 2999)
+            {
+                MessageBox.Show("imposto deve ser 2,5%");
+            }
+            else if (valordanotafiscal >= 3000 && valordanotafiscal <= 6999)
+            {
+                MessageBox.Show("imposto deve ser 2,8%");
+            }
+            else
+            {
+                MessageBox.Show("imposto deve ser 3%");
+            }
+               
+            
+
+
+        }
+
+    }
+}
