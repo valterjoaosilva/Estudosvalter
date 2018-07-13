3fae73602b7d388e8d4067d4afad22eedc2ec11a
V008 <V008@BNU-ESTAG008>
2018-07-13 17:07:53 -0300

    nova  classe contas

---
+++ T1/classecontas/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace classecontas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contas Valter = new contas();
+            Valter.numero = 1;
+            Valter.titular = "Valter João";
+            Valter.saldo = 3000.0;
+            Valter.cpf = "71264531915";
+
+
+            MessageBox.Show(Valter.titular);
+
+        }
+    }
+}
---
+++ T1/classecontas/contas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classecontas
+{
+    class contas
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+        public string cpf;
+    }
+}
