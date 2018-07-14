a2c9ce48bd843a5139f4825a028d4f507edb969e
Valter João Silva <valterjoaosilva@gmail.com>
2018-07-13 23:46:35 -0300

    sacar e depositar

---
+++ T1/contassacardepositar/Bancos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contassacardepositar
+{
+    class Bancos
+    {
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        public void deposita; (double valoradepositar)
+        {
+            if (valoradepositar > 0)
+            {
+            this.saldo += valoradepositar;
+            }
+        public void saca; (double valorsacado)
+    }
+
+}        
+        
+
+    
+
+
