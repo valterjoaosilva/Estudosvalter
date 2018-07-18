1054ac94fbecea28d18c5ba8d074cd914ec96988
V008 <V008@BNU-ESTAG008>
2018-07-18 16:49:36 -0300

    incluido botão para deposito

--- T1/Banco/Form1.Designer.cs
+++ T1/Banco/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.textoNumero = new System.Windows.Forms.TextBox();
             this.textoValor = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textoTitular
@@ -81,11 +82,22 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(308, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(185, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textoValor);
             this.Controls.Add(this.textoNumero);
@@ -106,6 +118,7 @@
         private System.Windows.Forms.TextBox textoNumero;
         private System.Windows.Forms.TextBox textoValor;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- T1/Banco/Form1.cs
+++ T1/Banco/Form1.cs
@@ -12,22 +12,22 @@ namespace Banco
 {
     public partial class Form1 : Form
     {
+        private Conta contaValter;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load( object sender, EventArgs e)
         {
-            Conta contaValter = new Conta();
+            contaValter = new Conta();
             contaValter.Numero = 1;
             contaValter.Titular = "Valter João"; 
             contaValter.deposita(250.0);
-            textoTitular.Text = contaValter.Titular;
-            textoNumero.Text = Convert.ToString(contaValter.Numero);
-            textoSaldo.Text = Convert.ToString(contaValter.Saldo);
-            String textoValorDeposito = textoValor.Text;
-            double valorDeposito = Convert.ToDouble(textoValorDeposito);
+
+            MostraConta();
+                     
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -51,6 +51,23 @@ namespace Banco
         }
 
         private void button1_Click(object sender, EventArgs e)
+        {
+            String textoValorDeposito = textoValor.Text;
+            double valorDeposito = Convert.ToDouble(textoValorDeposito);
+            contaValter.deposita(valorDeposito);
+
+            MostraConta();
+        }   
+
+        private void MostraConta()
+        {
+            textoTitular.Text = contaValter.Titular;
+            textoNumero.Text = Convert.ToString(contaValter.Numero);
+            textoSaldo.Text = Convert.ToString(contaValter.Saldo);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
