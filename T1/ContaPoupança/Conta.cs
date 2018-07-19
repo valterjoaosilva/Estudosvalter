6b2a4271809614b8ec0a4db09f2682e437075558
Valter João Silva <valterjoaosilva@gmail.com>
2018-07-18 22:37:05 -0300

    conta poupança polimorfismo

---
+++ T1/ContaPoupança/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaPoupança
+{
+    class Conta
+    {
+        public string Titular;
+        public double Saldo { get; protected set; }
+        public int Numero;
+
+        public virtual void  deposita(double Valor)
+        {
+            if (Valor > 0)
+            {
+                this.Saldo += Valor;
+            }
+        }
+        public void saca(double Valor)
+        {
+            if (this.Saldo > Valor)
+            {
+                this.Saldo -= Valor;
+            }
+
+        }
+        public void transfere(double Valor, Conta destino)
+        {
+            this.saca(Valor);
+            destino.deposita(Valor);
+        }
+    }
+}
---
+++ T1/ContaPoupança/ContaPoupaça.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaPoupança
+{
+    class ContaPoupaça : Conta
+    {
+        public virtual void deposita(double valor)
+        {
+            this.Saldo += valor + 0.10;
+        }
+    }
+}
