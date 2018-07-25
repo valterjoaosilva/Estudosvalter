da9d1d0ed44a565c9f7e3f64accf2538ac3a3b12
Valter João Silva <valterjoaosilva@gmail.com>
2018-07-24 22:26:19 -0300

    Alteração leitura de dados do terminal

---
+++ T1/Aplicação/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Aplicação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader leitor = Console.In;
+            string linha = leitor.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = leitor.ReadLine();
+            }
+        }
+    }
+}
