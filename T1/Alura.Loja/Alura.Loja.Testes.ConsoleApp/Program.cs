25d6109da30efb61052eb70a5f9d29f41cf09409
V008 <V008@BNU-ESTAG008>
2018-07-27 17:44:00 -0300

    produzindo promoção, ainda com erros

--- T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -10,6 +10,14 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Pascoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+            promocaoDePascoa.Produto.Add(new Produto());
+            promocaoDePascoa.Produto.Add(new Produto());
+            promocaoDePascoa.Produto.Add(new Produto());
+
             //compra de 6 pães franceses
             var paoFrances = new Produto();
             paoFrances.Nome = "Pão Francês";
---
+++ T1/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Promocao.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Promocao
+    {
+        public string Descricao { get; internal set; }
+        public DateTime DataInicio { get; internal set; }
+        public DateTime DataTermino { get; internal set; }
+        public object Produto { get; internal set; }
+    }
+}
