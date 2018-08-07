6c9945ec5e20e8d6e606ac3a635f656098a8a71d
V008 <V008@BNU-ESTAG008>
2018-08-07 17:20:55 -0300

    projeto inicial de mvc

---
+++ T1/CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,22 @@
+﻿using CaelumEstoque.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.Models;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Catergorias = categorias;
+            return View();
+        }
+    }
+}
---
+++ T1/Inicial/Controllers/homeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inicial.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+            // código da regra de negocio
+            return View();
+        }
+    }
+}
