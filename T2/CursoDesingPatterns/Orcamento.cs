b1de5e2a44980c0118f11de3556be1a269c24758
V008 <V008@BNU-ESTAG008>
2018-08-15 17:40:16 -0300

    Criando o calculador de desconto

--- T2/CursoDesingPatterns/Orcamento.cs
+++ T2/CursoDesingPatterns/Orcamento.cs
@@ -9,12 +9,18 @@ namespace CursoDesingPatterns
     public class Orcamento
     {
         public double Valor { get; private set; }
-
+        public IList<Item> itens { get; private set: }
         public Orcamento(double valor)
         {
-            this.Valor = valor; 
+            this.Valor = valor;
+            this.itens = new List<Item>();
+
         }
         
+        public void AdicionaItem(Item item)
+        {
+            itens.Add(item);
+        }
         public Orcamento() { }
 
     }
