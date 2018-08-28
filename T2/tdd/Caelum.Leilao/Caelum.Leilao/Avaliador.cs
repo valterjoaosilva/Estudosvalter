72ca9bc7c1e3f7e734db8d4a7da953378f52718b
V008 <V008@BNU-ESTAG008>
2018-08-28 16:40:42 -0300

    tdd inicio

---
+++ T2/tdd/Caelum.Leilao/Caelum.Leilao/Avaliador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    public class Avaliador
+    {
+        private double maiorDeTodos = Double.MinValue;
+        private double menorDeTodos = Double.MaxValue;
+        public void Avalia(Leilao leilao)
+        {
+            foreach (var lance in leilao.Lances)
+            {
+                if (lance.Valor > maiorDeTodos)
+                {
+                    maiorDeTodos = lance.Valor;
+                }
+                else if (lance.Valor < menorDeTodos)
+                {
+                    menorDeTodos = lance.Valor;
+                }
+            }
+        }
+        public double MaiorLance
+        {
+            get { return maiorDeTodos; }
+        }
+        public double MenorLance
+        {
+            get { return menorDeTodos; }
+        }
+    }
+}
---
+++ T2/tdd/Caelum.Leilao/Caelum.Leilao/TesteDoAvaliador.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caelum.Leilao
+{
+    public class TesteDoAvaliador
+    {
+        static void Main(String[] args)
+        {
+            Usuario joao = new Usuario("Joao");
+            Usuario jose = new Usuario("Jose");
+            Usuario maria = new Usuario("Maria");
+
+            Leilao leilao = new Leilao("Playstation 3 Novo");
+
+            leilao.Propoe(new Lance(maria, 250.0));
+            leilao.Propoe(new Lance(joao, 300.0));
+            leilao.Propoe(new Lance(jose, 400.0));
+            
+
+            Avaliador leiloeiro = new Avaliador();
+            leiloeiro.Avalia(leilao);
+
+            Console.WriteLine(leiloeiro.MaiorLance);
+            Console.WriteLine(leiloeiro.MenorLance);
+
+            Console.ReadKey();
+        }
+    }
+}
