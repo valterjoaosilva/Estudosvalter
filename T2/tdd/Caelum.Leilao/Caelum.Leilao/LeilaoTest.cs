2c017836d63afe7b0009fcb25107a03af4f01a9b
V008 <V008@BNU-ESTAG008>
2018-08-30 17:33:52 -0300

    leilão mac book

---
+++ T2/tdd/Caelum.Leilao/Caelum.Leilao/LeilaoTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class LeilaoTest
+    {
+        [Test]
+        public void DeveReceberUmLance()
+        {
+            Leilao leilao = new Leilao("macbook pro 15");
+            Assert.AreEqual(0, leilao.Lances.Count);
+
+            var usuario = new Usuario("Steve Jobs");
+            leilao.Propoe(new Lance(usuario, 2000));
+            Assert.AreEqual(1, leilao.Lances.Count);
+            Assert.AreEqual(2000, leilao.Lances[0].Valor, 0.00001);
+        }
+        [Test]
+        public void DeveReceberVariosLances()
+        {
+
+            Leilao leilao = new Leilao("macbook pro 15");
+            leilao.Propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+            leilao.Propoe(new Lance(new Usuario("Steve Wozniak"), 3000));
+
+            Assert.AreEqual(2, leilao.Lances.Count);
+            Assert.AreEqual(2000, leilao.Lances[0].Valor, 0.0001);
+            Assert.AreEqual(3000, leilao.Lances[1].Valor, 0.0001);
+
+        }
+
+    }
+}
