21602a6bd297269042694d142d31f5d48cdbced2
V008 <V008@BNU-ESTAG008>
2018-09-10 17:33:33 -0300

    segunda aula reflexion

---
+++ ByteBank.Portal/ByteBank.Portal/Infraestrutura/utilidades.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura
+{
+    public static class Utilidades
+    {
+
+        public static string ConverterParthParaNomeAssembly(string path)
+        {
+            // /Assets/css/styles.css
+            // /Assets/js/main.js
+
+            var prefixoAssembly = "ByteBank.Portal";
+            var pathComPontos = path.Replace('/', '.');
+
+            var nomeCompleto = $"{prefixoAssembly}.{pathComPontos}";
+
+            return nomeCompleto;
+        }
+            
+    }
+}
