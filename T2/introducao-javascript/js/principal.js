35d0f4b244b1376018b232ceae6aa96e948ac1e4
V008 <V008@BNU-ESTAG008>
2018-09-14 16:02:37 -0300

    incluido tabela para colocar paciente

--- T2/introducao-javascript/js/principal.js
+++ T2/introducao-javascript/js/principal.js
@@ -47,10 +47,40 @@ for(var i = 0; i < pacientes.length; i++){
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click",function(event){
     event.preventDefault();
-    console.log("Oi eu sou o botao e fui clicado")
+    
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
    
 });
-titulo.addEventListener("click", function (){
-    console.log("fui clicadio")
-});
 
+titulo.addEventListener("click",function(){
+    consoe.log("fui clicado");
+});
