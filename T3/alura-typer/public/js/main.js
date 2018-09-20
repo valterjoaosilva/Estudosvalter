218214543ad1caadcb1691b2466ae872fe041349
V008 <V008@BNU-ESTAG008>
2018-09-20 16:33:37 -0300

    controle do tempo de digitação

--- T3/alura-typer/public/js/main.js
+++ T3/alura-typer/public/js/main.js
@@ -1,1 +1,22 @@
-console.log("ola mundo")
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+var tamanhoFrase = $("#tamanho-frase");
+
+tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input",function(){
+    var conteudo = campo.val();
+
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+});
+var tempoRestante = $("#tempo-digitacao");
+campo.on("focus", function(){
+
+});
